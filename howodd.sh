53e94808f85323a154b40abbe139cbb5fddb854d
carolynzy <carolynzy@hotmail.com>
2017-11-21 12:05:51 +0800

    clean other files

--- howodd.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-function howodd {
-	source /mnt/c/Users/yang/Documents/coursera/nevens.sh
-	let number=`nevens $@`
-	percentage=`echo $number / $#|bc -l`
-	odds=`expr 1 - $percentage`
-	echo $odds
-}
